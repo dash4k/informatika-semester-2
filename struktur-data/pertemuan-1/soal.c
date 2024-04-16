e231b4e365dc310b66e7c6a6b6ff81affca9110c
dash4k <133938416+dash4k@users.noreply.github.com>
2024-04-16 23:36:51 +0800

    updated

--- struktur-data/pertemuan-1/soal.c
+++ struktur-data/pertemuan-1/soal.c
@@ -6,37 +6,38 @@
 // struct mahasiswa
 typedef struct mahasiswa
 {
-    int NIM;
+    char no_peserta[50];
     char nama[50];
-    int uas;
-    int uts;
+    int teori;
+    int praktek;
     int nilai;
     int rata_rata;
     bool status;
 }mahasiswa_t;
 
+// struct array mahasiswa
+typedef struct array
+{
+    mahasiswa_t *data;
+    int total_data;
+}array_t;
+
 
 // utilities function definition
 mahasiswa_t create_mahasiswa();
-void print_mahasiswa(mahasiswa_t *mahasiswa);
-void print_hasil(mahasiswa_t *mahasiswa, int x);
+array_t *create_array(int len);
+void print_mahasiswa(array_t *mahasiswa);
 
 // main function
 int main(void){
     system("cls");
-    int x;
+    int len;
     printf("Masukkan Jumlah Mahasiswa: ");
-    scanf("%d", &x);
+    scanf("%d", &len);
 
-    mahasiswa_t *array_mahasiswa = malloc(sizeof(mahasiswa_t) * x);
+    array_t *array = create_array(len);
 
-    for (int i = 0; i < x; i++)
-    {
-        array_mahasiswa[i] = create_mahasiswa();
-        printf("\n");
-    }
-
-    print_hasil(array_mahasiswa, x);
+    print_mahasiswa(array);
 }
 
 // create mahasiswa function
@@ -44,18 +45,22 @@ mahasiswa_t create_mahasiswa(){
 
     mahasiswa_t mahasiswa;
 
-    printf("Masukkan NIM: ");
-    scanf("%d", &mahasiswa.NIM);
-    printf("Masukkan Nama: ");
     getchar();
+    printf("Masukkan Nomor Peserta: ");
+    fgets(mahasiswa.no_peserta, sizeof(mahasiswa.no_peserta), stdin);
+    mahasiswa.no_peserta[strcspn(mahasiswa.no_peserta, "\r\n")] = 0;
+
+    printf("Masukkan Nama: ");
     fgets(mahasiswa.nama, sizeof(mahasiswa.nama), stdin);
     mahasiswa.nama[strcspn(mahasiswa.nama, "\r\n")] = 0;
-    printf("Masukkan Nilai UAS: ");
-    scanf("%d", &mahasiswa.uas);
-    printf("Masukkan Nilai UTS: ");
-    scanf("%d", &mahasiswa.uts);
 
-    mahasiswa.nilai = mahasiswa.uas + mahasiswa.uts;
+    printf("Masukkan Nilai Teori: ");
+    scanf("%d", &mahasiswa.teori);
+
+    printf("Masukkan Nilai Praktek: ");
+    scanf("%d", &mahasiswa.praktek);
+
+    mahasiswa.nilai = mahasiswa.teori + mahasiswa.praktek;
     
     mahasiswa.rata_rata = (mahasiswa.nilai / 2);
 
@@ -68,47 +73,47 @@ mahasiswa_t create_mahasiswa(){
         mahasiswa.status = false;
     }
 
+    printf("\n");
+
     return mahasiswa;
 }
 
-// print mahasiswa function
-void print_mahasiswa(mahasiswa_t *mahasiswa){
-    printf("\n\n");
-    printf("NIM: %d \n", mahasiswa->NIM);
-    printf("Nama: %s \n", mahasiswa->nama);
-    printf("UAS: %d \n", mahasiswa->uas);
-    printf("UTS: %d \n", mahasiswa->uts);
-    printf("Nilai: %d \n", mahasiswa->nilai);
-    printf("Rata-Rata: %d \n", mahasiswa->rata_rata);
+// create array mahasiswa function
+array_t *create_array(int len){
+    array_t *temp = malloc(sizeof(array_t));
+    temp->total_data = len;
+    temp->data = calloc(len, sizeof(mahasiswa_t));
 
-    if (mahasiswa->status)
+    for (int i = 0; i < temp->total_data; i++)
     {
-        printf("Status: LULUS");
-    }
-    else
-    {
-        printf("Status: GAGAL");
+        temp->data[i] = create_mahasiswa();
     }
     
-    
+    return temp;
 }
 
-// print hasil function
-void print_hasil(mahasiswa_t *mahasiswa, int x){
-
-    mahasiswa_t *temp = mahasiswa;
-    printf("\nHasil: \n");
-    for (int i = 0; i < x; i++)
+// print mahasiswa function
+void print_mahasiswa(array_t *mahasiswa){
+    system("cls");
+    for (int i = 0; i < mahasiswa->total_data; i++)
     {
-        if (temp[i].status)
+        printf("Nomor Peserta: %s \n", mahasiswa->data[i].no_peserta);
+        printf("Nama: %s \n", mahasiswa->data[i].nama);
+        printf("Nilai Teori: %d \n", mahasiswa->data[i].teori);
+        printf("Nilai Praktek: %d \n", mahasiswa->data[i].praktek);
+        printf("Nilai: %d \n", mahasiswa->data[i].nilai);
+        printf("Rata-Rata: %d \n", mahasiswa->data[i].rata_rata);
+
+        if (mahasiswa->data[i].status)
         {
-            printf("%s LULUS \n", temp[i].nama);
+            printf("Status: LULUS\n");
         }
         else
         {
-            printf("%s GAGAL \n", temp[i].nama);
+            printf("Status: GAGAL\n");
         }
-        
+
+        printf("\n");
     }
     
 }
