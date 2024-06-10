16cf5e5d1b39005b9016d5d83ee49242672bee9a
Danishwara Pracheta <danishwara81@gmail.com>
2024-06-10 11:14:45 +0800

    minor bug fix

--- diskrit-2/graph_python/graph_algorithm/dijkstra.py
+++ diskrit-2/graph_python/graph_algorithm/dijkstra.py
@@ -1,7 +1,7 @@
 from graph import Graph
 
 def find_lowest(dict = dict, visited = [], graph = Graph) -> int:
-    temp = 999999999
+    temp = 1e4
     temp_index = 0
     for i in range(graph.size):
         if temp > dict[i] and not visited[i]:
@@ -10,13 +10,13 @@ def find_lowest(dict = dict, visited = [], graph = Graph) -> int:
     return temp_index
 
 def dijkstra_algorithm(graph = Graph, target = int) -> dict:
-    short_path = {i: 99999 for i in range(graph.size)}
+    short_path = {i: 1e4 for i in range(graph.size)}
     visited = [False for i in range(graph.size)]
     short_path[target] = 0
-    for i in range(graph.size):
+    for _ in range(graph.size):
         min_vertex = find_lowest(short_path, visited, graph)
         visited[min_vertex] = True
         for j in range(graph.size):
-            if graph.adjMatrix[i][j] > 0 and not visited[j] and short_path[j] > short_path[i] + graph.adjMatrix[i][j]:
-                short_path[j] =  short_path[i] + graph.adjMatrix[i][j]
+            if graph.adjMatrix[min_vertex][j] > 0 and not visited[j] and short_path[j] > short_path[min_vertex] + graph.adjMatrix[min_vertex][j]:
+                short_path[j] =  short_path[min_vertex] + graph.adjMatrix[min_vertex][j]
     return short_path
--- diskrit-2/graph_python/graph_algorithm/main.py
+++ diskrit-2/graph_python/graph_algorithm/main.py
@@ -1,20 +1,37 @@
 from graph import Graph
 from dijkstra import dijkstra_algorithm
 
-size = int(input("Enter the total number of vertices: "))
+# size = int(input("Enter the total number of vertices: "))
+
+size = 9
 
 graph = Graph(size)
 
-print("Enter an integer if the vertices are connected, 0 if not connected")
-i = 0
-while i < size:
-    j = 0
-    while j < size:
-        temp = int(input(f"\t{i} -> {j}: "))
-        if temp != 0:
-            graph.connect_vertices(i, j, temp)
-        j += 1
-    i += 1
+# print("Enter an integer if the vertices are connected, 0 if not connected")
+# i = 0
+# while i < size:
+#     j = 0
+#     while j < size:
+#         temp = int(input(f"\t{i} -> {j}: "))
+#         if temp != 0:
+#             graph.connect_vertices(i, j, temp)
+#         j += 1
+#     i += 1
+
+# graph.print_graph()
+
+# print("\n\n")
+
+graph.adjMatrix = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+           [4, 0, 8, 0, 0, 0, 0, 11, 0],
+           [0, 8, 0, 7, 0, 4, 0, 0, 2],
+           [0, 0, 7, 0, 9, 14, 0, 0, 0],
+           [0, 0, 0, 9, 0, 10, 0, 0, 0],
+           [0, 0, 4, 14, 10, 0, 2, 0, 0],
+           [0, 0, 0, 0, 0, 2, 0, 1, 6],
+           [8, 11, 0, 0, 0, 0, 1, 0, 7],
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]
+           ]
 
 graph.print_graph()
 
